10ae7f036bd10c7f77695ef64dbbbaf3a4e96e67
Permik <tintti214@gmail.com>
2020-08-24 02:51:41 +0300

    Added unit tests to hexStringToByteArray. Ensures that upper and lowercase input works.

--- app/src/main/java/xyz/santtu/materialcarrot/Util.kt
+++ app/src/main/java/xyz/santtu/materialcarrot/Util.kt
@@ -94,10 +94,10 @@ private val HEX_CHARS = "0123456789abcdef"
 fun String.hexStringToByteArray() : ByteArray {
 
     val result = ByteArray(length / 2)
-    //val string = this.toLowerCase(Locale.ROOT)
+    val string = this.toLowerCase(Locale.ROOT)
     for (i in 0 until length step 2) {
-        val firstIndex = HEX_CHARS.indexOf(this[i])
-        val secondIndex = HEX_CHARS.indexOf(this[i + 1])
+        val firstIndex = HEX_CHARS.indexOf(string[i])
+        val secondIndex = HEX_CHARS.indexOf(string[i + 1])
 
         val octet = firstIndex.shl(4).or(secondIndex)
         result.set(i.shr(1), octet.toByte())
--- app/src/test/java/xyz/santtu/materialcarrot/UtilUnitTest.kt
+++ app/src/test/java/xyz/santtu/materialcarrot/UtilUnitTest.kt
@@ -5,8 +5,10 @@ import org.junit.Test
 class UtilUnitTest {
     @Test
     fun toHexValidator_AllValues_ReturnsTrue() {
-        // it seems that hex can be calculated with normal ints by adding 34 to it every time
-        //val lookupTable = listOf<Int>(1,35,69,103,137,171,205,239)
+        // it seems that a bytearray of
+        // "0123 4567 89ab cdef"
+        // can be calculated with normal ints by adding 34 to it every time
+        // val lookupTable = listOf<Int>(1,35,69,103,137,171,205,239)
         val test = ByteArray(8) { x: Int -> (1+(x*34)).toByte() }
         assert(toHex(test) == "0123456789abcdef")
     }
@@ -15,6 +17,12 @@ class UtilUnitTest {
     fun hexStringToByteArray_AllValues_ReturnsTrue() {
         val compareTo = ByteArray(8) { x: Int -> (1+(x*34)).toByte() }
         assert("0123456789abcdef".hexStringToByteArray().contentEquals(compareTo))
+    }
+
+    @Test
+    fun hexStringToByteArray_ConvertsCapitalInput_ReturnsTrue() {
+        val compareTo = "0123456789abcdef".hexStringToByteArray()
+        assert("0123456789ABCDEF".hexStringToByteArray().contentEquals(compareTo))
     }
 
     // b56e aaec a79f 9518
