10126cdc45e2ae7ce66401fef4232649e5566197
Permik <tintti214@gmail.com>
2020-09-26 03:58:22 +0300

    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         maven("https://dl.bintray.com/kotlin/kotlin-eap")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0-alpha10")
+        classpath("com.android.tools.build:gradle:4.2.0-alpha12")
         classpath(kotlin("gradle-plugin", version = "1.4.0"))
 
         // NOTE: Do not place your application dependencies here; they belong
