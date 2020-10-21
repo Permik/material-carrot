07d68c73bc86158afec4329c63a181b1f5de5b45
Permik <tintti214@gmail.com>
2020-10-21 21:19:15 +0300

    Updated deps.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
         maven("https://dl.bintray.com/kotlin/kotlin-eap")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0-alpha13")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.0")
+        classpath("com.android.tools.build:gradle:4.2.0-alpha14")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.1")
         classpath(kotlin("gradle-plugin", version = "1.4.0"))
 
         // NOTE: Do not place your application dependencies here; they belong
